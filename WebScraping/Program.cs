ab97de50859e6a553e6b9b49b17a0e97f6161ddd
Marcel <52543642+Marcel-Sanchez@users.noreply.github.com>
2022-11-14 18:36:34 -0500

    Initial commit with go to url and fill inputs

---
+++ WebScraping/Program.cs
@@ -0,0 +1,37 @@
+﻿using cloudscribe.HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Interactions;
+using WebScraping;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(Setting.Url);
+
+            var rut1 = driver.FindElement(By.Name(Setting.NameRutField1));
+            var rut2 = driver.FindElement(By.Name(Setting.NameRutField2));
+            var accept = driver.FindElement(By.Name(Setting.NameAcceptField));
+
+            rut1.SendKeys(Setting.Rut1Value);
+            rut2.SendKeys(Setting.Rut2Value);
+
+            accept.Submit();
+
+        }
+    }
+}
---
+++ WebScraping/Setting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraping
+{
+    internal class Setting
+    {
+        public static string Url = "https://palena.sii.cl/cvc/dte/ee_empresas_dte.html";
+
+        public static string NameRutField1 = "RUT_EMP";
+        public static string NameRutField2 = "DV_EMP";
+        public static string NameAcceptField = "ACEPTAR";
+
+        public static string Rut1Value = "76749144";
+        public static string Rut2Value = "1";
+    }
+}
